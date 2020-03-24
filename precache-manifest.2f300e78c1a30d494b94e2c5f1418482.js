e66b8846a9277d7e2779d4deba2c3d2361e52ee2
AhmadButt00 <ahmadbutt00@hotmail.com>
2020-03-24 18:46:41 +0500

    Updates

---
+++ precache-manifest.2f300e78c1a30d494b94e2c5f1418482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335aac4cc4a46db28b503459672be274",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "d2cb1e93cac7610ecfe2",
+    "url": "/Covid-Tracker/static/css/main.edc631f4.chunk.css"
+  },
+  {
+    "revision": "9247305382df06b8881f",
+    "url": "/Covid-Tracker/static/js/2.548e3cf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid-Tracker/static/js/2.548e3cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cb1e93cac7610ecfe2",
+    "url": "/Covid-Tracker/static/js/main.662a74b7.chunk.js"
+  },
+  {
+    "revision": "82834432cdf452bbfdfb",
+    "url": "/Covid-Tracker/static/js/runtime-main.6230a259.js"
+  }
+]);
